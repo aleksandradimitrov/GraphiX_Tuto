2dfb480367fb9aaaf34668ed1d8acf50b0963195
Aleksandra <dimcaka@gmail.com>
2023-03-29 13:23:33 +0300

    unocss

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
+import '@unocss/reset/tailwind-compat.css';
+import 'uno.css';
 import './bootstrap';
-import '../css/app.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
---
+++ unocss.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'unocss/vite'
+
+import { presetAttributify, presetUno, presetIcons, presetTypography } from 'unocss'
+import {presetDaisy} from 'unocss-preset-daisy'
+import { presetForms } from '@julr/unocss-preset-forms'
+
+export default defineConfig({
+    presets: [
+        presetForms(),
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+            scale: 1.3,
+        }),
+        presetTypography(),
+        presetDaisy(),
+    ]
+})
