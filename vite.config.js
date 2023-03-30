a2840e1eaccb77322ef1e0e534a490c0580ff31e
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-03-30 21:12:25 +0300

    Add eslint and lint the code

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
-
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-        UnoCSS(),
-    ],
-});
+  plugins: [
+    laravel({
+      input: 'resources/js/app.js',
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+    UnoCSS(),
+  ],
+})
