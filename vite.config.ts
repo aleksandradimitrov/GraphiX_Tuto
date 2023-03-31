af8212041e3f92cf1024558e2d2142a68d7fbb38
IvoKara <ivo.i.karaneshev@gmail.com>
2023-03-31 04:28:39 +0300

    Fix frontend entry point

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
+
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
     laravel({
-      input: 'resources/js/app.js',
+      input: 'resources/js/app.ts',
       refresh: true,
     }),
     vue({
@@ -17,6 +22,38 @@ export default defineConfig({
           includeAbsolute: false,
         },
       },
+    }),
+    Components({
+      dirs: [
+        './resources/js/Layouts',
+        './resources/js/Components',
+        './resources/js/Components/**/*',
+        './resources/js/Pages/**/Partials',
+      ],
+      directoryAsNamespace: true,
+      types: [
+        {
+          from: '@inertiajs/vue3',
+          names: ['Head', 'Link'],
+        },
+      ],
+
+    }),
+    AutoImport({
+      dirs: [
+        './resources/js/Composables/',
+        './resources/shared/Composables/',
+      ],
+      imports: [
+        'vue',
+        '@vueuse/core',
+        {
+          '@inertiajs/vue3': ['router', 'useForm', 'Link', 'usePage'],
+          '@inertiajs/core': ['Errors', 'ErrorBag'],
+        },
+      ],
+      vueTemplate: true,
+
     }),
     UnoCSS(),
   ],
